39cdf706380f243cfe255f441858987b45b72054
Chris <caquinn.86@gmail.com>
2020-02-07 09:04:56 -0800

    finished project

--- data/migrations/20200207084455_car-schema.js
+++ data/migrations/20200207084455_car-schema.js
@@ -5,6 +5,7 @@ exports.up = function(knex) {
         tbl.text('vin', 128).unique().notNullable();
         tbl.text('make', 128).notNullable();
         tbl.text('model', 128).notNullable();
+        tbl.decimal('mileage', 128).notNullable();
         tbl.text('transmission', 128);
         tbl.text('title', 128);
     })
